9276f09c414c50db408805ebdb4b362a3bd374dc
ughsxl <101698689+ughsxl@users.noreply.github.com>
2023-10-22 22:02:02 +0300

    init commit

---
+++ app/src/main/java/com/cryptocurrency/chess_tournaments/presentation/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.cryptocurrency.chess_tournaments.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.cryptocurrency.chess_tournaments.R
+
+class MainActivity : AppCompatActivity() {
+	override fun onCreate(savedInstanceState: Bundle?) {
+		super.onCreate(savedInstanceState)
+		setContentView(R.layout.activity_main)
+	}
+}
